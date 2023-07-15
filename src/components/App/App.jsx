b42afc835188ccef36953d3f511032f5854d69a3
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-07-15 17:20:40 -0500

    Installed dependencies of redux, react-redux, and react-logger. | Configured Redux in index.js. Imported, using provider, set up feedback reducer state with object with properties of empty strings, and set up store with logger. | In App.js, imported React router DOM, and useSelector to call in feedback redux state for console.log test.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,27 @@
+// IMPORTS
 import React from 'react';
-import axios from 'axios';
 import './App.css';
+// Axios for server requests
+import axios from 'axios';
+// React router DOM
+import { HashRouter as Router, Route } from "react-router-dom";
+// Redux store state selector
+import { useSelector } from 'react-redux/es/hooks/useSelector.js';
 
 function App() {
 
+  const feedback = useSelector(store => store.feedback);
+  console.log("feedback state is:", feedback);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+      </div>
+    </Router>
   );
 }
 
