1130eb9bc3ef7fd0beb234884d587a48f34a4070
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-07-16 02:05:52 -0500

    In App.js, imported and added link to Feedback header to send use to home page. | In Review component, Imported redux feedback state to display feedback answer values. O . Click of submit button, sending feedback to server via POST request, dispatching action to clear feedback state in reducer, sending user 'ThankYou' component client route. | ThankYou component has h1 header and will have functionality to take user new feedback form.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,23 @@
 // - IMPORTS -
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // * Axios for server requests
-import axios from 'axios';
+import axios from "axios";
 // * React router DOM
 import { HashRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 // * Redux store state selector
-import { useSelector } from 'react-redux/es/hooks/useSelector.js';
+import { useSelector } from "react-redux/es/hooks/useSelector.js";
 // * Components
-import Feeling from '../Feeling/Feeling.jsx';
-import Understanding from '../Understanding/Understanding.jsx';
-import Support from '../Support/Support.jsx';
-import Comments from '../Comments/Comments.jsx';
-import Review from '../Review/Review.jsx';
+import Feeling from "../Feeling/Feeling.jsx";
+import Understanding from "../Understanding/Understanding.jsx";
+import Support from "../Support/Support.jsx";
+import Comments from "../Comments/Comments.jsx";
+import Review from "../Review/Review.jsx";
+import ThankYou from "../ThankYou/ThankYou.jsx";
+
 
 function App() {
-  
   const feedback = useSelector((store) => store.feedback);
   console.log("feedback state is:", feedback);
 
@@ -24,9 +26,9 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <Route exact path="/">
-          <h1 className="App-title">Feedback!</h1>
-          </Route>
+          <Link to="/">
+            <h1 className="App-title">Feedback!</h1>
+          </Link>
           <h4>Don't forget it!</h4>
         </header>
 
@@ -53,6 +55,11 @@ function App() {
         {/* Review Component; Fifth Page */}
         <Route exact path="/review">
           <Review />
+        </Route>
+        
+        {/* ThankYou Component; Sixth Page */}
+        <Route exact path="/thankYou">
+          <ThankYou />
         </Route>
       </div>
     </Router>
---
+++ src/components/ThankYou/ThankYou.jsx
@@ -0,0 +1,10 @@
+
+
+export default function ThankYou() {
+
+    // Send user to home page using useHistory
+    return(
+        <h2>Thank You!</h2>
+    )
+
+} // end ThankYou
